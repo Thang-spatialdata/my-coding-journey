33460bd0eef474d6e8b7bd687472cefede0f387e
Thang-spatialdata <thangqn2023@gmail.com>
2026-07-20 03:15:20 +0700

    Add dictionary example for city information

---
+++ Day13/dictionary.py
@@ -0,0 +1,10 @@
+# Day 13: Practicing Python Dictionary
+city = {
+    "name": "Hung Yen",
+    "population": 125000,
+    "area": 930.2
+}
+
+for key, value in city.items():
+     print(f"{key} : {value}")
+  
