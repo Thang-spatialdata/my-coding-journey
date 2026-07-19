0f0846fc6a705afa860ef4b7557e6b0acb03be7c
Thang-spatialdata <thangqn2023@gmail.com>
2026-07-20 02:55:20 +0700

    Add dictionary loop to print city details

---
+++ day12_dictionary_loops.py
@@ -0,0 +1,10 @@
+# Day 12: Practicing Python Dictionary
+city = {
+    "name": "Ha Noi",
+    "population": 125000,
+    "area": 930.2
+}
+
+for key, value in city.items():
+     print(f"{key} : {value}")
+  
