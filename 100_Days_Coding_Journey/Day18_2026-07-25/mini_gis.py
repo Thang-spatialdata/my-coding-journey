62080137bffe33158f0f87fc9cf5f32e83dea786
Thang-spatialdata <thangqn2023@gmail.com>
2026-07-26 00:12:26 +0700

    Create mini_gis.py

---
+++ 100_Days_Coding_Journey/Day18_2026-07-25/mini_gis.py
@@ -0,0 +1,17 @@
+# Day 18 - Mini GIS
+
+provinces = [
+    "Hung Yen",
+    "Ha Noi",
+    "Nam Dinh",
+    "Hai Phong",
+    "Hung Yen"
+]
+
+print(f"Number of provinces: {len(provinces)}")
+
+if "Ha Noi" in provinces:
+    print(True)
+
+print(provinces.count("Hung Yen"))
+print(provinces.index("Nam Dinh"))
