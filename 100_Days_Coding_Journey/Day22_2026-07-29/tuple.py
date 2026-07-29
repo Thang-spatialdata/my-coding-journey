f32490b60ffbf19ba8680208fb74d3264596a454
Thang-spatialdata <thangqn2023@gmail.com>
2026-07-29 23:32:41 +0700

    Add tuple.py with examples of tuple usage

---
+++ 100_Days_Coding_Journey/Day22_2026-07-29/tuple.py
@@ -0,0 +1,23 @@
+# Practice 1
+river = ("Red River", 1149, "Vietnam")
+
+print(river[0])
+print(river[1])
+print(river[2])
+
+# Practice 2
+hung_yen = (20.65, 106.05)
+
+latitude, longitude = hung_yen
+
+print(f"Latitude: {latitude}")
+print(f"Longitude: {longitude}")
+
+# Practice 3
+station = ("HY02", 31, "Tien Lu")
+
+code, temp, district = station
+
+print(f"Code: {code}")
+print(f"Temperature: {temp}°C")
+print(f"District: {district}")
